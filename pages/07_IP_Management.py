80f5d7fbcb5e54caea7b28f6e3d88c227eedbeaa
doglikevulturesBD <doglikevultures@gmail.com>
2025-12-03 19:24:33 +0200

    Update 07_IP_Management.py

--- pages/07_IP_Management.py
+++ pages/07_IP_Management.py
@@ -35,7 +35,7 @@ hero_html = """
 
 <div class="hero-content">
 <h1 class="hero-title">Intellectual Property Management</h1>
-<p class="hero-sub">Determine the strongest IP protection pathway for your innovation.</p>
+<p class="hero-sub"></p>
 </div>
 </div>
 """
